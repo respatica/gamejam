54d7abd95c93cf30f6e686b1ccca99bf4d7fc765
respatica <respatica@hotmail.com>
2023-09-07 20:41:27 -0400

    hdad

---
+++ Assets/scripts/MusicMaster.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(Slider))]
+public class MusicMaster : MonoBehaviour
+{
+    Slider slider { get{ return GetComponent<Slider>();}}
+
+    public AudioMixer mixer;
+    [SerializeField]
+    public string volumeName;
+    [SerializeField]
+    public Text volumeLabel;
+
+    private void Start(){
+        UpdateValueOnChange(slider.value);
+        slider.onValueChanged.AddListener(delegate{ UpdateValueOnChange(slider.value);});
+    }
+    public void UpdateValueOnChange(float value){
+        if (mixer!=null){
+          mixer.SetFloat(volumeName,Mathf.Log(value)*20f);  
+        }
+        if (volumeLabel!=null){
+            volumeLabel.text = Mathf.Round(value*100f).ToString()+"%";
+        }
+        
+    }
+    
+}
